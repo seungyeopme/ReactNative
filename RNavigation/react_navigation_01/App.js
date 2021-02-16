ca97b755e86ff63e78598ff540554cb362321823
seungyeopme <seungyeop.me.dev@gmail.com>
2021-02-16 21:33:37 +0900

    [Inflearn] React Native - Basic [Drawer] Custom Component 수강 완료

--- RNavigation/react_navigation_01/App.js
+++ RNavigation/react_navigation_01/App.js
@@ -7,18 +7,42 @@
  */
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import { StyleSheet, View, Text, Image, Button, Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { 
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem 
+} from '@react-navigation/drawer';
 import HomeScreen from './src/home';
 import UserScreen from './src/user';
 import LogoTitle from './src/logo';
 import DrawerHomeScreen from './src/home_drawer';
 import DrawerUserScreen from './src/user_drawer';
+import PictogramHome from './src/assets/pics/home_icon.png';
+import SideDrawer from './src/my_drawer';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+// CustomDrawerContent = (props) => {
+//   return (
+//     <DrawerContentScrollView {...props}>
+//       <DrawerItemList {...props}/>
+//       <DrawerItem
+//         label="Help"
+//         onPress={()=>Linking.openURL('http://www.google.com')}
+//         icon={()=><LogoTitle/>}
+//       /> 
+//       <DrawerItem
+//         label="Info"
+//         onPress={()=>alert('Info Window')}
+//       /> 
+//     </DrawerContentScrollView>
+//   )
+// }
 
 class App extends Component {
 
@@ -31,11 +55,36 @@ class App extends Component {
 //   )
 // }
 
+
   render () {
     return (
       <NavigationContainer>
-        <Drawer.Navigator>
-          <Drawer.Screen name="Home" component={DrawerHomeScreen}/>
+        <Drawer.Navigator
+          initialRouteName="Home"
+          drawerType="front"
+          drawerPosition='right'
+          drawerStyle={{
+            backgroundColor: '#c6cbef',
+            width: 200
+          }}
+          drawerContentOptions={{
+            activeTintColor: 'red',
+            activeBackgroundColor: 'skyblue'
+          }}
+          drawerContent={props => <SideDrawer {...props} />}
+        >
+          <Drawer.Screen 
+            name="Home" 
+            component={DrawerHomeScreen}
+            options={{
+              drawerIcon: () => (
+                <Image
+                  source={PictogramHome}
+                  style={{width: 40, height: 40}}
+                />
+              )
+            }}
+          />
           <Drawer.Screen name="User" component={DrawerUserScreen}/>
         </Drawer.Navigator>
       </NavigationContainer>
--- RNavigation/react_navigation_01/src/user_drawer.js
+++ RNavigation/react_navigation_01/src/user_drawer.js
@@ -7,10 +7,24 @@
  */
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Image } from 'react-native';
+import Logo from './assets/pics/home_icon.png';
 
 class DrawerUserScreen extends Component {
+
+  drawerStyle = () => {
+    this.props.navigation.setOptions({
+      drawerIcon: () => (
+        <Image
+          source={Logo}
+          style={{width: 40, height: 40}}
+        />
+      )
+    })
+  }
+
   render () {
+    this.drawerStyle();
     return (
         <View style={{
             flex: 1,
