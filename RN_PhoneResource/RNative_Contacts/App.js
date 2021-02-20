cc0dc447e6f6825684ed27c40af8d27fdc485317
seungyeopme <seungyeop.me.dev@gmail.com>
2021-02-20 14:01:09 +0900

    [Inflearn] React Native - Basic 연락처 Form 활용 수강완료

--- RN_PhoneResource/RNative_Contacts/App.js
+++ RN_PhoneResource/RNative_Contacts/App.js
@@ -11,6 +11,9 @@ import { Platform, PermissionsAndroid, StyleSheet, View, Text, Image, Button } f
 import Contacts from 'react-native-contacts';
 
 class App extends Component {
+  state = {
+    myContacts: []
+  }
 
   async requestContactPermission() {
     if (Platform.OS === 'ios') {
@@ -35,6 +38,22 @@ class App extends Component {
     }
   }
 
+  // getContacts = () => {
+  //   this.requestContactPermission()
+  //   .then((didGetPermission)=>{
+  //     if(didGetPermission) {
+  //       Contacts.getAll((err, contacts) => {
+  //         if(err) {
+  //           throw err;
+  //         }
+  //         console.warn(contacts)
+  //       })
+  //     } else {
+  //       alert('no permission')
+  //     }
+  //   })
+  // }
+
   getContacts = () => {
     this.requestContactPermission()
     .then((didGetPermission)=>{
@@ -43,7 +62,9 @@ class App extends Component {
           if(err) {
             throw err;
           }
-          console.warn(contacts)
+          this.setState({
+            myContacts: contacts
+          })
         })
       } else {
         alert('no permission')
@@ -51,13 +72,93 @@ class App extends Component {
     })
   }
 
+  addContacts = () => {
+    this.requestContactPermission()
+    .then((didGetPermission)=>{
+      if(didGetPermission) {
+
+        const newContact = {
+          emailAddress: [{
+            label: "work",
+            email: "aaa@example.com"
+          }],
+          familyName: "Go",
+          givenName: "Gildong",
+          phoneNumbers: [{
+            label: "mobile",
+            number: "(010) 1111-1111"
+          }]
+        }
+
+  //       Contacts.getAll((err, contacts) => {
+  //         if(err) {
+  //           throw err;
+  //         }
+  //         this.setState({
+  //           myContacts: contacts
+  //         })
+  //       })
+  //     } else {
+  //       alert('no permission')
+  //     }
+  //   })
+  // }
+
+        Contacts.addContacts(newContact, (err) => {
+          if(err) {
+            throw err;
+          }
+          this.getContacts();
+        })
+      } else {
+        alert('no permission')
+      }
+    })
+  }
+
+  openForm = () => {
+    const newContact = {
+      emailAddress: [{
+        label: "work",
+        email: "aaa@example.com"
+      }],
+      familyName: "ccccc",
+      givenName: "ddddd",
+      phoneNumbers: [{
+        label: "mobile",
+        number: "(010) 1111-1111"
+      }]
+    }
+
+    Contacts.openContactForm(newContact, (err) => {
+      if(err) console.warn(err)
+    })
+  }
 
   render () {
     return (
       <View style={styles.container}>
+        {
+          this.state.myContacts.map((item,idx)=>(
+            <Text key={idx}>
+              {item.givenName} {item.familyName}
+            </Text>
+          ))
+        }
+
         <Button
           title="Load Contacts"
           onPress={()=>this.getContacts()}
+        />
+
+        <Button
+          title="Add Contacts"
+          onPress={()=>this.addContacts()}
+        />
+
+        <Button
+          title="Open Form"
+          onPress={()=>this.openForm()}
         />
       </View>
     )
